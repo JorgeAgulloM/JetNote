41fde91280ac379bbe1c5286b67c996a777b5553
Jorge Agullo <agullojorge@gmail.com>
2022-05-13 11:32:53 +0200

    [FEATURE] Final NoteComponent.kt

--- app/src/main/java/com/practice/jetnote/components/NoteComponent.kt
+++ app/src/main/java/com/practice/jetnote/components/NoteComponent.kt
@@ -1,8 +1,44 @@
 package com.practice.jetnote.components
 
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
-fun NoteComponent(){
+fun NoteInputText(
+    modifier: Modifier = Modifier,
+    text: String,
+    label: String,
+    maxLine: Int,
+    onTextChange: (String) -> Unit,
+    onTimeAction: () -> Unit
+) {
+    val keyboardController = LocalSoftwareKeyboardController.current
 
+    TextField(
+        value = text,
+        onValueChange = onTextChange,
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = Color.Transparent //backgroundColor = Color.Transparent
+        ),
+        maxLines = maxLine,
+        label = { Text(text = label) },
+        keyboardOptions = KeyboardOptions.Default.copy(
+            imeAction = ImeAction.Done
+        ),
+        keyboardActions = KeyboardActions(onDone = {
+            onTimeAction() //onImeAction()
+            keyboardController?.hide()
+        }),
+        modifier = modifier
+    )
 }

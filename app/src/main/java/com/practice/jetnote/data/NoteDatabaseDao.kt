3c03865115ae193897975d914003997f37e7562c
Jorge Agullo <agullojorge@gmail.com>
2022-05-17 12:50:11 +0200

    [FEATURE] Added DAO with coroutines

---
+++ app/src/main/java/com/practice/jetnote/data/NoteDatabaseDao.kt
@@ -0,0 +1,27 @@
+package com.practice.jetnote.data
+
+import androidx.room.*
+import com.practice.jetnote.model.Note
+
+@Dao
+interface NoteDatabaseDao {
+
+    @Query("SELECT * FROM notes_tbl")
+    fun getNotes(): List<Note>
+
+    @Query("SELECT * FROM notes_tbl WHERE id =:id")
+    suspend fun getNoteById(id: String): Note
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(note: Note)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun update(note: Note)
+
+    @Query(value = "DELETE from notes_tbl")
+    suspend fun deleteAll()
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+}
